8043003170722823b902898df44c62f55d2ba22a
Saurabh Pathak <saurabhpathakce@gmail.com>
2016-08-04 17:50:55 +0530

    directory created

---
+++ arithmeticoperationlib/src/main/java/com/saurabh/arithmeticoplib/ArithmeticOperation.java
@@ -0,0 +1,39 @@
+package com.saurabh.arithmeticoplib;
+
+/**
+ * Created by saurabh on 4/8/16.
+ */
+public class ArithmeticOperation {
+
+    public long addition(long a, long b) {
+        return a + b;
+    }
+
+    public double addition(double a, double b) {
+        return a + b;
+    }
+
+    public long subtration(long a, long b) {
+        return a - b;
+    }
+
+    public double subtration(double a, double b) {
+        return a - b;
+    }
+
+    public long multiplication(long a, long b) {
+        return a * b;
+    }
+
+    public double multiplication(double a, double b) {
+        return a * b;
+    }
+
+    public long division(long a, long b) {
+        return a / b;
+    }
+
+    public double division(double a, double b) {
+        return a / b;
+    }
+}
